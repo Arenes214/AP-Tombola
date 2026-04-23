bfac67bebcae3e6e19d40e6b014d02450c2ff32b
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-04-23 20:01:41 +0200

    beautify save slots part 1

--- login/saveslot.gd
+++ login/saveslot.gd
@@ -3,6 +3,11 @@ signal login_from_save(slot_name: String, ip: String, port: String, password: St
 signal delete_save_request(save_name: String)
 
 var save_name: String
+var slot_name: String
+var ip: String
+var port: String
+var password: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,19 +18,26 @@ func _process(delta: float) -> void:
 	pass
 
 func load_info(info: Dictionary):
-	$VBox/SaveName.text = info["save_name"]
+	%VBox/SaveName.text = info["save_name"]
 	save_name = info["save_name"]
 	
-	$VBox/SlotName.text = info["slot_name"]
-	$VBox/IP.text = info["ip"]
-	$VBox/Port.text = info["port"]
-	$VBox/Password.text = info["password"]
+	%VBox/SlotName.text = info["slot_name"]
+	slot_name = info["slot_name"]
+	
+	%VBox/IP.text = info["ip"]
+	ip = info["ip"]
+	
+	%VBox/Port.text = info["port"]
+	port = info["port"]
+	
+	%VBox/Password.text = info["password"]
+	password = info["password"]
 	
 	
 
 func _on_load_button_pressed() -> void:
-	login_from_save.emit($VBox/SlotName.text, $VBox/IP.text, $VBox/Port.text, $VBox/Password.text)
+	login_from_save.emit(slot_name, ip, port, password)
 
 
 func _on_delete_button_pressed() -> void:
-	delete_save_request.emit($VBox/SaveName.text)
+	delete_save_request.emit(save_name)
